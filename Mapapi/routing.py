887344fccdcf30722634fc04c2980e26a71fa24e
immersir <129412569+immerSIR@users.noreply.github.com>
2026-06-28 22:59:33 -0400

    Fix WS incident routes: <int:> -> <uuid:incident_id>

--- Mapapi/routing.py
+++ Mapapi/routing.py
@@ -5,6 +5,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/notifications/', consumers.NotificationConsumer.as_asgi()),
-    path('ws/incidents/<int:incident_id>/discussion/', consumers.DiscussionConsumer.as_asgi()),
-    path('ws/incidents/<int:incident_id>/tasks/', consumers.TaskConsumer.as_asgi()),
+    path('ws/incidents/<uuid:incident_id>/discussion/', consumers.DiscussionConsumer.as_asgi()),
+    path('ws/incidents/<uuid:incident_id>/tasks/', consumers.TaskConsumer.as_asgi()),
 ]
